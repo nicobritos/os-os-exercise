6f50493fd294732eb22af75cf0b62bfb4a65e48b
igrasso98 <40865466+igrasso98@users.noreply.github.com>
2019-10-27 10:52:39 -0300

    Add files via upload

--- TPE/Kernel/include/process.h
+++ TPE/Kernel/include/process.h
@@ -20,38 +20,40 @@ typedef struct
     t_state state; 
     void * stackPointer;
     void * processMemoryLowerAddress;
-    void * processMemoryUpperAddress;
-    int (*entryPoint)(int,char **); //a chequear
     }t_process;
 
 typedef struct 
     {
-      //uint16_t gs;
-      //uint16_t fs;
-	    uint64_t r15;
-	    uint64_t r14;
-	    uint64_t r13;
-	    uint64_t r12;
-	    uint64_t r11;
-	    uint64_t r10;
-	    uint64_t r9;
-	    uint64_t r8;
-	    uint64_t rsi;
-	    uint64_t rdi;
-	    uint64_t rbp;
-	    uint64_t rdx;
-	    uint64_t rcx;
-	    uint64_t rbx;
-	    uint64_t rax;
-	    uint64_t rip;
-	    //uint16_t cs;
-	    uint64_t rflags;
-	    uint64_t rsp;
-	    //uint16_t ss;
+        uint64_t gs;
+        uint64_t fs;
+        uint64_t r15;
+        uint64_t r14;
+        uint64_t r13;
+        uint64_t r12;
+        uint64_t r11;
+        uint64_t r10;
+        uint64_t r9;
+        uint64_t r8;
+        uint64_t rsi;
+        uint64_t rdi;
+        uint64_t rbp;
+        uint64_t rdx;
+        uint64_t rcx;
+        uint64_t rbx;
+        uint64_t rax;
+        uint64_t rip;
+        uint64_t cs;
+        uint64_t eflags;
+        uint64_t rsp;
+        uint64_t ss;
+        uint64_t base;
+
     } t_stack;
 
-    t_process * createProcess(char * name, int (*entry)(int, char**), int pPid, int argc, char * argv[]);
-    void initializeStack(t_stack * stackFrame, int (*entryPoint)(int, char**), void * startingPoint, int argc, char * argv[]);
-    void deleteProcess(t_process * process);
-    void freeProcess(t_process process);
+    t_process * createProcess(char * name, void* startingPoint,int ppid, int argc, char * argv[]);
+    void processWrapper(int argc, char * argv[], void * startingPoint);
+    void initializeStack(t_stack * stackFrame, int argc, char * argv[], void * startingPoint);
+    void freeProcess(t_process * process);
+
+
 #endif
